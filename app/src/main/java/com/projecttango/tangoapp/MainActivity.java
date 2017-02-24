a30dc4fca61b0a8f47f9304b42eb5c765e87a060
sriramya04 <ramya.nimmagadda77@gmail.com>
2017-02-24 12:23:24 -0800

    Filters

--- app/src/main/java/com/projecttango/tangoapp/MainActivity.java
+++ app/src/main/java/com/projecttango/tangoapp/MainActivity.java
@@ -1,10 +1,18 @@
-//Created by Ramya 11/14/2016
+//Created by Ramya on 11/4/2016
 package com.projecttango.tangoapp;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.SurfaceView;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
@@ -15,12 +23,14 @@ import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.imgproc.Imgproc;
 
+import static org.opencv.core.Core.BORDER_DEFAULT;
+
 public class MainActivity extends Activity implements CameraBridgeViewBase.CvCameraViewListener2 {
 
-    private  static final String TAG = "MainActivity";
+    private static final String TAG = "MainActivity";
     JavaCameraView javaCameraView;
-    Mat mRgba ,imgGray, imgCanny;
-    BaseLoaderCallback mLoaderCallBack = new BaseLoaderCallback(this){
+    Mat mRgba, imgGray, imgCanny, imgSobel, imgthresh;
+    BaseLoaderCallback mLoaderCallBack = new BaseLoaderCallback(this) {
         @Override
         public void onManagerConnected(int status) {
             switch (status) {
@@ -37,42 +47,89 @@ public class MainActivity extends Activity implements CameraBridgeViewBase.CvCam
     static {
 
     }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        javaCameraView = (JavaCameraView)findViewById(R.id.java_camera_view);
+
+        javaCameraView = (JavaCameraView) findViewById(R.id.JavaCameraView);
+        final RelativeLayout rl = (RelativeLayout) findViewById(R.id.rl);
         javaCameraView.setVisibility(SurfaceView.VISIBLE);
         javaCameraView.setCvCameraViewListener(this);
 
+        Button button = (Button) findViewById(R.id.capture_button);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View arg0) {
+                // Do something in response to button click
+
+                // Take the screenshot
+
+                Bitmap screenShot = TakeScreenShot(rl);
+
+                // Save the screenshot on device gallery
+                MediaStore.Images.Media.insertImage(
+                        getContentResolver(),
+                        screenShot,
+                        "Image.png",
+                        "Captured ScreenShot"
+                );
+
+                // Notify the user that screenshot taken.
+                Toast.makeText(getApplicationContext(), "Screen Captured", Toast.LENGTH_LONG).show();
+
+
+            }
+        });
     }
+
+
+
+
+    private Bitmap TakeScreenShot(View JavaCameraView) {
+        // Screenshot taken for the specified root view and its child elements.
+
+        Bitmap bitmap = Bitmap.createBitmap(JavaCameraView.getWidth(), JavaCameraView.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        JavaCameraView.draw(canvas);
+        return bitmap;
+    }
+
+
     @Override
-    protected void onPause(){
+    protected void onPause() {
         super.onPause();
-        if(javaCameraView!=null)
+        if (javaCameraView != null)
             javaCameraView.disableView();
     }
 
     @Override
-    protected void onResume(){
+    protected void onResume() {
         super.onResume();
-        if(!OpenCVLoader.initDebug()){
+        if (!OpenCVLoader.initDebug()) {
             Log.d(TAG, "OpenCV  loaded successfully");
             mLoaderCallBack.onManagerConnected(LoaderCallbackInterface.SUCCESS);
         } else {
             Log.d(TAG, "OpenCV loaded");
-            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_1_0,this,mLoaderCallBack);
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_1_0, this, mLoaderCallBack);
         }
 
     }
 
     @Override
     public void onCameraViewStarted(int width, int height) {
+
         mRgba = new Mat(height, width, CvType.CV_8UC4);
         imgGray = new Mat(height, width, CvType.CV_8UC1);
         imgCanny = new Mat(height, width, CvType.CV_8UC1);
-
+        imgSobel = new Mat(height, width, CvType.CV_8UC1);
+        imgthresh = new Mat(height, width, CvType.CV_8UC1);
 
     }
 
@@ -84,10 +141,30 @@ public class MainActivity extends Activity implements CameraBridgeViewBase.CvCam
 
     @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
-        mRgba=inputFrame.rgba();
-        Imgproc.cvtColor(mRgba,imgGray, Imgproc.COLOR_RGB2GRAY);
-        //Imgproc.Canny(imgGray,imgCanny,50,150);
-        return imgGray;
-    }
-}
+        mRgba = inputFrame.rgba();
+        // Converts RGB to Gray scale iamge
 
+        Imgproc.cvtColor(mRgba, imgGray, Imgproc.COLOR_RGB2GRAY);
+
+
+
+        //Canny is for detection of edges especially in noise state by applying thresholding method.
+        Imgproc.Canny(imgGray, imgCanny, 50, 150);
+
+        // Sobel operator is it can detect edges and their orientations.
+        //Sobel( input img, output img, ddepth, x_order, y_order, scale, delta, BORDER_DEFAULT );
+        Imgproc.Sobel(imgGray, imgSobel, -1, 0, 1, 3, 1, 0, BORDER_DEFAULT);
+
+        //Thresolding means dividing the complete image into a set of pixels in such a way that the pixels in each set have some common characteristics.
+        // Image segmentation is highly useful in defining objects and their boundaries.
+        Imgproc.threshold(imgGray, imgthresh, 127, 255, Imgproc.THRESH_BINARY);
+
+        return imgthresh;
+    }
+
+
+
+
+
+
+}
